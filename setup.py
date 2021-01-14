a14bd4f1b5e0d624392185af53a1ee93f9630b01
Aryan <74603055+RefinedDev@users.noreply.github.com>
2021-01-14 11:45:13 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="twitpy",
+    version="1.0.0",
+    description="A package based on the twitter api, search users of twitter and MORE!.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires = ['tweepy'],
+    python_requires='>=3.6',
+)
